4e022be8f6dd16227d6f15c3aad525db303eabbf
Madhu Sivapragasam <madhuran.siva@gmail.com>
2021-01-16 00:26:43 -0500

    Updating gitignore

--- Components/EventForm.js
+++ Components/EventForm.js
@@ -23,14 +23,14 @@ export default function EventForm() {
               placeholder="Event name"
               onChangeText={props.handleChange("name")}
               value={props.values.name}
-              style={styles.input}
+              style={styles.input1}
             ></TextInput>
 
             <TextInput
               placeholder="Event time"
               onChangeText={props.handleChange("time")}
               value={props.values.time}
-              style={styles.input}
+              style={styles.input2}
             ></TextInput>
             <Button title="submit"></Button>
           </SafeAreaView>
@@ -44,7 +44,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  input: {
-    marginTop: 50,
+  input1: {
+    marginTop: 40,
+    borderColor: "grey",
+    height: 50,
+    width: 400,
+    marginLeft: 5,
+    borderWidth: 5,
+    paddingLeft: 10,
+    borderRadius: 10,
+  },
+
+  input2: {
+    marginTop: 10,
+    borderColor: "grey",
+    height: 50,
+    width: 400,
+    marginLeft: 5,
+    borderWidth: 5,
+    paddingLeft: 10,
+    marginBottom: 5,
+    borderRadius: 10,
   },
 });
